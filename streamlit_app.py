27179b77f4fae062ae037daed3336374c78a858b
Suma Sarvani <44917914+Ishhhaa@users.noreply.github.com>
2022-09-16 11:11:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,10 @@ streamlit.title('My Parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 &vBluberry Oatmeal')
+streamlit.text('🥣Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-boiled free-range egg')
+streamlit.text('🥑🍞avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
